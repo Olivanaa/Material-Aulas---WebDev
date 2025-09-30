8a2bb24f4871e4394329b079cbe5dc75731e34a1
olivanaa <oliv.anaclara@gmail.com>
2025-09-29 22:31:15 -0300

    aula 2909

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Dashboard.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Dashboard.jsx
@@ -1,10 +1,28 @@
+import { GraficoBarras } from "@/components/GraficoBarras";
 import Tabs from "../components/Tabs";
+import { Button } from "@/components/ui/button";
+
 
 export default function Dashboard() {
+
+  function handleClick(){
+    alert("clicou!")
+  }
+
   return (
+    <>
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold mb-4">Bem-vindo ao Dashboard</h1>
       <Tabs />
+      <GraficoBarras />
     </div>
+    {/* <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button onClick={handleClick} className="bg-purple-300 cursor-pointer">Button</Button>
+    </div> */}
+    </>
   );
 }
+
+// <Button onClick={() => alert("Clicou!")} className="bg-purple-300 cursor-pointer">Button</Button> - função precisa estar dentro de uma arrow function para não executar a função sem clicar
+// Um indicador-chave de desempenho (KPI) é um tipo de métrica utilizada para medir, comparar e acompanhar o desempenho de algo.
+// shadcn - componentes
--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
@@ -6,3 +6,4 @@ export default function Relatorios() {
     </div>
   );
 }
+
